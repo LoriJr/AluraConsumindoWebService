d1059f87519be3cac0aabcc237c5d94f0a1d2e6d
lorijr <jrsilva.lorivas@gmail.com>
2025-09-08 08:37:00 -0300

    chore: add class principalcombusca

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o filme que deseja: ");
+        String busca = sc.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=71262be4";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
