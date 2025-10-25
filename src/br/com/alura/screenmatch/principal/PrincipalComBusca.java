3450bd55aa50d1629f994c02428f62015e2c5d06
pc asus <jrsilva.lorivas@gmail.com>
2025-10-25 11:46:22 -0300

    add TituloOmdb

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,10 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,7 +19,7 @@ public class PrincipalComBusca {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o filme que deseja: ");
         String busca = sc.nextLine();
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=71262be4";
+        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=71262be4";
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -25,6 +30,13 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println("Título: " + tituloOmdb);
+        Titulo meuTitulo = new Titulo(tituloOmdb);
+        System.out.println("Titulo convertido: " + meuTitulo);
     }
 }
